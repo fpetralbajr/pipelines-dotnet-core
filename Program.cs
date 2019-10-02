9e3ac4b0cb840a4f8bee142cd4630e567a15ec0f
Felipe Petralba <petralfaf@gmail.com>
2019-10-02 12:27:15 +0800

    test commit all

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-// test 20191002   lesson on commit    more
+// test 20191002   lesson on commit    more pa jud small
 namespace pipelines_dotnet_core
 {
     public class Program
