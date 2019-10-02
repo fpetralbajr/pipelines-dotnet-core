351648561cd1a5c53cbadfa12f680949ab84c65d
Felipe Petralba <petralfaf@gmail.com>
2019-10-02 14:08:37 +0800

    commit

---
+++ Models/ErrorViewModel_bak.cs
@@ -0,0 +1,11 @@
+using System;
+// test by ipe
+namespace pipelines_dotnet_core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
